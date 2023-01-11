9941f98af3db64671c51aa287834f02c2e8858ea
bibibricodeur <bibibricodeur@gmail.com>
2023-01-11 06:54:20 +0100

    WIP add brave-browser

--- mint-post-install.sh
+++ mint-post-install.sh
@@ -83,6 +83,15 @@ if (whiptail --title "Post installer" --yesno "Développer ?" 15 45); then
     #***
 	sudo apt-get install brasero gimp handbrake filezilla git inkscape nmap nodejs npm printrun samba sqlitebrowser rpi-imager tree vokoscreen whois -y
     #***
+    
+    #***
+    # Brave
+    # https://brave.com/linux/#release-channel-installation
+    # ***
+    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+    sudo apt update
+    sudo apt install brave-browser  
 
     #***
     # pip3
